735c1f9dff05b29d7eb4ad782a4637331125ab4c
Donato Wolfisberg <donato.wolfisberg@gmail.com>
2018-11-06 11:20:21 +0100

    added component test

---
+++ angular-lingua-lib/projects/angular-lingua-lib/src/lib/component/translate.component.spec.ts
@@ -0,0 +1,115 @@
+import {Component, DebugElement} from '@angular/core';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {MatIcon, MatIconModule} from '@angular/material';
+import {By} from '@angular/platform-browser';
+import {TranslationModule} from '../translation.module';
+import {Translation} from '../translation.type';
+
+@Component({
+  template: `
+    <translate [key]="key"></translate>`
+})
+class TestTranslateComponent {
+  key: Translation;
+}
+
+@Component({
+  template: `
+    <translate [key]="key">
+      <mat-icon *translateParams="'FLAG'">flag</mat-icon>
+    </translate>
+  `
+})
+class TestTranslateWithParamsComponent {
+  public key: Translation;
+}
+
+
+describe('TranslateComponent', () => {
+  const defaultLang = 'deu';
+  describe('TranslateComponent without Parameter', () => {
+    let fixture: ComponentFixture<TestTranslateComponent>;
+    let component: TestTranslateComponent;
+    let translateEl: DebugElement;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [TestTranslateComponent],
+        imports: [TranslationModule.forRoot(defaultLang)]
+      });
+
+      fixture = TestBed.createComponent(TestTranslateComponent);
+      component = fixture.componentInstance;
+      translateEl = fixture.debugElement.query(By.css('translate'));
+    });
+
+    it('should translate text', () => {
+      const smTranslation = 'Hello World';
+      component.key = {[defaultLang]: smTranslation};
+      fixture.detectChanges();
+
+      expect(translateEl.nativeElement.textContent.trim()).toBe(smTranslation);
+    });
+
+    it('should throw error', () => {
+      component.key = {};
+
+      expect(
+        () => fixture.detectChanges()
+      ).toThrowError(/.*translation not found.*/);
+    });
+
+  });
+
+  describe('TranslateComponent with Parameter', () => {
+    let fixture: ComponentFixture<TestTranslateWithParamsComponent>;
+    let component: TestTranslateWithParamsComponent;
+    let translateEl: DebugElement;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [TestTranslateWithParamsComponent],
+        imports: [TranslationModule.forRoot(defaultLang), MatIconModule]
+      });
+
+      fixture = TestBed.createComponent(TestTranslateWithParamsComponent);
+      component = fixture.componentInstance;
+      translateEl = fixture.debugElement.query(By.css('translate'));
+    });
+
+    it('should translate text', () => {
+      const smTranslation = 'Hello World';
+      component.key = {[defaultLang]: smTranslation};
+      fixture.detectChanges();
+
+      expect(translateEl.nativeElement.textContent.trim()).toBe(smTranslation);
+    });
+
+    it('should throw error', () => {
+      component.key = {};
+
+      expect(
+        () => fixture.detectChanges()
+      ).toThrowError(/.*translation not found.*/);
+    });
+
+    it('should put element in translation', () => {
+      const smTranslation = 'This is a {{FLAG}}';
+      component.key = {[defaultLang]: smTranslation};
+      fixture.detectChanges();
+
+      expect(translateEl.query(By.css('mat-icon'))).toBeTruthy();
+    });
+
+    it('should throw error can not replace variable', () => {
+      const smTranslation = 'This is a {{NOT_EXISTING}}';
+      component.key = {[defaultLang]: smTranslation};
+
+      expect(
+        () => fixture.detectChanges()
+      ).toThrowError('could not find Key: NOT_EXISTING');
+    });
+
+
+  });
+});
