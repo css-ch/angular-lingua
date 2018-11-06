45fb1e27f4e8f4b93e6dc71e9ca6fee8bca9dab5
Donato Wolfisberg <donato.wolfisberg@gmail.com>
2018-11-06 10:35:29 +0100

    created an e2e test

--- angular-lingua-lib/e2e/src/app.e2e-spec.ts
+++ angular-lingua-lib/e2e/src/app.e2e-spec.ts
@@ -1,14 +1,23 @@
-import { AppPage } from './app.po';
+import {browser} from 'protractor';
+import {AppPage} from './app.po';
 
-describe('workspace-project App', () => {
+describe('random test', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to angular-lingua-lib!');
+  it('should not log any errors', async () => {
+    await page.navigateTo();
+
+    for (let i = 0; i < 100; i++) {
+      await page.clickRandomButton();
+    }
+
+    browser.manage().logs().get('browser').then(function (browserLog) {
+      console.log(JSON.stringify(`broswer log: ${JSON.stringify(browserLog)}`));
+      expect(browserLog.length).toBe(0);
+    });
   });
 });
--- angular-lingua-lib/e2e/src/app.po.ts
+++ angular-lingua-lib/e2e/src/app.po.ts
@@ -1,11 +1,18 @@
-import { browser, by, element } from 'protractor';
+import {browser, by, element, protractor} from 'protractor';
 
 export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  async clickRandomButton() {
+    const elements = element.all(by.css('a, button:not(.sidenav-toggle)'));
+    const count = await elements.count();
+
+    const button = elements.get(Math.floor(Math.random() * count));
+
+    await browser.wait(protractor.ExpectedConditions.elementToBeClickable(button), 1000);
+
+    await button.click();
   }
 }
