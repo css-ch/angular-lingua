0657f18c9b4c4409128515c7aa456a00a99c961c
Donato Wolfisberg <donato.wolfisberg@gmail.com>
2019-08-06 10:26:00 +0200

    update dependencies and fixed security alerts

--- angular-lingua-lib/src/test.ts
+++ angular-lingua-lib/src/test.ts
@@ -2,7 +2,7 @@
 
 import {getTestBed} from '@angular/core/testing';
 import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
-import 'core-js/es7/reflect';
+import 'core-js/features/reflect';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/zone-testing';
 
