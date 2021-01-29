6ddfed822dcea24dc320b87fc44c5c912b1b84e6
Donato Wolfisberg <donato@wolfisberg.dev>
2021-01-29 09:10:48 +0000

    added puppeteer

--- angular-lingua-lib/karma.conf.js
+++ angular-lingua-lib/karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -19,6 +21,13 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, 'coverage'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
+    },
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessCustom: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
